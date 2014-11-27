4fbed27759894832abe68cf75946eca6ae8a5204
Ian Reinhart Geiser <igeiser@devonit.com>
2014-11-27 13:39:54 -0500

    start of autogenerate fontawesome.

---
+++ make_awesome.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+from fontTools.ttLib import TTFont, TTLibError
+import codecs
+import os
+import sys
+
+js_template_header = """
+.pragma library
+
+var map = {
+"""
+
+js_template_footer = """
+};
+"""
+
+javascriptOut = codecs.open(sys.argv[2], encoding='utf-8', mode='w')
+
+javascriptOut.write(js_template_header)
+
+ttf = TTFont(sys.argv[1], fontNumber=0)
+for code,name in list( ttf['cmap'].getcmap(3,1).cmap.items() ):
+    javascriptOut.write(u"\t'%s' : '%s',\n" % (name, unichr(code)))
+
+javascriptOut.write(js_template_footer)
+javascriptOut.close()
--- qml-material.pro
+++ qml-material.pro
@@ -5,7 +5,15 @@ deployment.files += qmldir \
                     icons \
                     fonts \
                     ListItems \
-                    Transitions 
+                    Transitions
 
 deployment.path = $$[QT_INSTALL_QML]/Material
 INSTALLS += deployment
+
+OTHER_FILES += $$deployment.files
+
+mytarget.target = awesome.js
+mytarget.commands = ${IN_PWD}/make_awesome.py ${PWD}/fonts/fontawesome/FontAwesome.otf $$mytarget.target
+mytarget.CONFIG += no_link target_predeps ignore_no_exist
+
+QMAKE_EXTRA_TARGETS += mytarget
