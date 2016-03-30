b096ed555c73bf5add1623b746e6d2fabdf356e4
Michael Spencer <sonrisesoftware@gmail.com>
2016-03-30 18:04:10 -0500

    chore: Relicense all of the core classes under the MPL 2.0

--- src/core/awesome.js
+++ src/core/awesome.js
@@ -1,3 +1,12 @@
+/*
+ * QML Material - An application framework implementing Material Design.
+ *
+ * Copyright (C) 2016 Michael Spencer <sonrisesoftware@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 .pragma library
 
